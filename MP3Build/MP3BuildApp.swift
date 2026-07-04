7d41937c6963804481c581701c2ad42ac44e8d00
ray1yawn <shussain2@s207.org>
2026-07-03 22:59:17 -0500

    fix: avoid remote events during app startup

--- MP3Build/MP3BuildApp.swift
+++ MP3Build/MP3BuildApp.swift
@@ -1,11 +1,9 @@
 import SwiftUI
-import UIKit
 
 @main
 struct MP3BuildApp: App {
     init() {
         FontRegistry.registerMinecraftFont()
-        UIApplication.shared.beginReceivingRemoteControlEvents()
     }
 
     var body: some Scene {
